8bd23486829eff537fac35353449572aaec5095e
vgianluca96 <gianluca.vallese@hotmail.com>
2023-08-25 14:46:12 +0200

    versione 1
    scritta logica di verifica se i è multiplo di 3, 5 o entrambi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 
 
+for (let i= 1; i <= 100; i++) {
+    
+    // Logica per verificare se i è multiplo di 3, 5 o entrambi
+    if (i%3 == 0) {
+        if (i%5 == 0) {
+            console.log('FizzBuzz');
+        } else {
+            console.log('Fizz');
+        }
+    } else if (i%5 == 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
 
+}
